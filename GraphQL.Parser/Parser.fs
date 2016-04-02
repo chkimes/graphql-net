7c979e8070131f97ddaf184acfcfb834eca4cd4a
Robert Peele <robert.s.peele@gmail.com>
2016-04-02 15:15:07 -0400

    Add new AST types and a parser for them using FParsec-Pipes.

---
+++ GraphQL.Parser/Parser.fs
@@ -0,0 +1,580 @@
+﻿module private GraphQL.Parser.Parser
+open GraphQL.Parser.ParserAST
+open System
+open System.Collections.Generic
+open System.Globalization
+open System.Text.RegularExpressions
+open FParsec
+open FParsec.Pipes
+
+(**
+
+This code implements the parser for GraphQL.
+
+It is based on the grammar summary in appendix B of the draft RFC specification.
+We begin with ignored tokens. The spec goes into detail here
+which it would be counterproductive to try to reproduce in 1-for-1 detail,
+since FParsec's `CharStream` class already handles things like normalizing newlines
+and skipping over Unicode byte order marks.
+*)
+
+let comment =
+    %% '#' -- restOfLine false -%> ()
+
+let isIgnoredCharacter c =
+    c = '\t'
+    || c = ' '
+    || c = ','
+
+// Parses a run of ignored tokens of similar type.
+let ignoredToken =
+    %[
+        // Either a run of spaces/commas
+        skipMany1Satisfy isIgnoredCharacter
+        // A newline
+        skipNewline
+        // Or a line comment
+        comment
+    ]
+
+// Parses ignored tokens in the stream, if any.
+let ignored = skipMany ignoredToken
+
+(**
+
+We'll frequently be allowing ignored tokens in between other parsers,
+so let's define an inline operator to make this easier.
+
+*)
+
+/// Chain `parser` onto `pipe` with ignored tokens allowed to precede it.
+let inline (-..-) pipe parser = pipe -- ignored -- parser
+
+(**
+
+Before we move on to literal values, we need to add a few more utilities.
+A practical parser should always include source information (at least line numbers)
+in the parsed AST, so that code consuming the AST can can produce useful warnings or
+errors when it encounters constructs that are semantically invalid, like type mismatches
+or unresolved variable names.
+
+We've defined our own types to carry this information, so that callers don't need to directly reference
+FParsec's `Position` type. Here, we implement parsers that help consume those types.
+
+*)
+
+// Translates from FParsec's position type to our own.
+let sourcePosition =
+    %% +.p<Position>
+    -%> fun pos -> { Index = pos.Index; Line = pos.Line; Column = pos.Column }
+
+// Wraps any parser with source information.
+let withSource (parser : Parser<'a, unit>) =
+    %% +.sourcePosition
+    -- +.parser
+    -- +.sourcePosition
+    -%> fun startPos value endPos ->
+        {
+            Source = { StartPosition = startPos; EndPosition = endPos }
+            Value = value
+        }
+
+(**
+
+Now let's consume the `Value` part of the AST.
+
+Several different parsers consume identifiers.
+An identifier could be a variable name, an enum value, or field name in an object.
+The rules for these names are simple enough. They must begin with an underscore
+or alphanumeric character. After the first character, digits are also permitted.
+> Name :: /[_A-Za-z][_0-9A-Za-z]*/
+*)
+
+// Parses an identifier name.
+let name =
+    let isInitial c =
+        c = '_'
+        || c >= 'A' && c <= 'Z'
+        || c >= 'a' && c <= 'z'
+    let isFollowing c =
+        isInitial c
+        || c >= '0' && c <= '9'
+    many1Satisfy2 isInitial isFollowing
+
+(**
+
+Variables are simply names prefixed with a "$" sign.
+We will not include the "$" sign in the AST, considering it
+to be purely syntax rather than a part of the name.
+
+*)
+
+let variableName = 
+    %% '$' -- +.name -%> auto
+
+let variable =
+    %% +.variableName -%> Variable
+
+(**
+
+Both float and int parsers can be implemented to match the spec
+by way of FParsec's `numberLiteral` parser.
+We use some extra validation to ensure that zero does not appear before another
+digit in the integer part of the literal, as required by the spec.
+
+*)
+
+let numericValue =
+    let numberOptions =
+        NumberLiteralOptions.AllowMinusSign
+        ||| NumberLiteralOptions.AllowExponent
+        ||| NumberLiteralOptions.AllowFraction
+    let invalidLeadingZero = new Regex(@"^-?0[0-9]")
+    numberLiteral numberOptions "numeric literal"
+    >>= fun literal ->
+        if invalidLeadingZero.IsMatch(literal.String) then
+            literal.String
+            |> sprintf "Non-zero numeric literal (%s) may not start with a 0"
+            |> fail
+        else if literal.IsInteger then
+            literal.String |> Int64.Parse |> IntValue |> preturn
+        else
+            literal.String |> Double.Parse |> FloatValue |> preturn
+
+(**
+
+String literals are comparable to those in JavaScript.
+Strings consist of runs of 0 or more normal characters
+separated by escaped characters. FParsec includes a `stringSepBy`
+to parse this type of sequence efficiently.
+
+*)
+
+let stringValue =
+    let isRegularCharacter c =
+        c <> '"'
+        && c <> '\''
+        && c <> '\n'
+    let regularCharacters = manySatisfy isRegularCharacter
+    let escapedCharacter =
+        let unicode (hex4 : char array) =
+            char <| Int32.Parse(new String(hex4), NumberStyles.HexNumber)
+        %% '\\'
+        -- +.[
+            %% 'u' -- +.(4, hex) -%> unicode
+            % '"'
+            % '\\'
+            %% 'b' -%> '\b'
+            %% 'f' -%> '\x0C' // form feed
+            %% 'n' -%> '\n'
+            %% 'r' -%> '\r'
+            %% 't' -%> '\t'
+        ]
+        -%> auto
+    let escapedCharacters = many1Chars escapedCharacter
+    %% '"'
+    -- +.stringsSepBy regularCharacters escapedCharacters
+    -- '"'
+    -%> StringValue
+
+(**
+
+Boolean literals are of course trivial to parse.
+Interestingly, GraphQL has no null literal --
+if it did, it would be parsed much the same way.
+
+*)
+
+let booleanValue =
+    %[
+        %% "true" -%> BooleanValue true
+        %% "false" -%> BooleanValue false
+    ]
+
+(**
+
+Enum values can be any name that is not a boolean value or null.
+We can avoid parsing boolean values by prioritizing them ahead of enum
+values in the parser. Null must be manually checked for, however, since
+there is no null literal.
+
+*)
+
+let enumValue =
+    name >>= function
+    | "null" -> fail "null is not a legal enum value name"
+    | x -> preturn (EnumValue x)
+
+(**
+
+In order to define list values, we need a complete value parser.
+Since we haven't defined the parser unifying all values yet,
+we'll take it as an argument.
+
+*)
+
+let listValue (value : Parser<Value, _>) =
+    %% '['
+    -..- +.(qty.[0..] /.ignored * withSource value)
+    -- ']'
+    -%> ListValue
+
+(**
+
+Object values are nearly identical to list values, but include property names.
+
+*)
+
+let objectValue (value : Parser<Value, _>) =
+    let sourceValue = withSource value
+    let objectField =
+        %% +.name
+        -..- ':'
+        -..- +.sourceValue
+        -%> auto
+    %% '{'
+    -..- +.(qty.[0..] /. ignored * objectField)
+    -- '}'
+    -%> (dict >> ObjectValue)
+
+(**
+
+Now that we have all the different types of values defined, they can be unified into
+a general value parser with a recursive definition.
+
+*)
+
+let value = precursive <| fun value ->
+    %[
+        variable
+        numericValue
+        stringValue
+        booleanValue
+        enumValue
+        listValue value
+        objectValue value
+    ]
+
+(**
+
+The spec also defines a "const" variant of the value parser, which excludes variables
+and lists/objects containing variables. Because our list and object parsers are parameterized
+this is trivial to implement.
+
+*)
+
+let valueConst = precursive <| fun valueConst ->
+    %[
+        numericValue
+        stringValue
+        booleanValue
+        enumValue
+        listValue valueConst
+        objectValue valueConst
+    ]
+
+(**
+
+Now that we have values, let's keep building up to full queries.
+
+An argument is a named value.
+It is found in an argument list, which looks like this:
+
+    (arg1: value1, arg2: value2)
+
+Remember, however, that the commas are entirely optional, since they are ignored tokens.
+
+*)
+
+/// Parses an argument list wrapped in parentheses. Per the GraphQL spec,
+/// this list must contain at least one argument.
+let arguments =
+    let argument =
+        %% +.name
+        -..- ':'
+        -..- +.value
+        -%> fun name value ->
+            { ArgumentName = name; ArgumentValue = value }
+    %% '(' -..- +.(qty.[1..] /. ignored * withSource argument) -- ')'
+    -%> auto
+
+(**
+
+The spec on directives explains that they can be used to describe additional
+information for fields, fragments, and operations.
+
+A directive looks like this:
+
+    @dirName
+
+Or, with arguments:
+
+    @dirName(arg1: value1, arg2: value2)
+
+*)
+
+/// Runs the given list parser, which parses a list of one or more elements,
+/// or returns an empty list. This is useful when a list is optional but must
+/// contain at least one item if it is present, which is common in GraphQL.
+let optionalMany parser =
+    %[
+        parser
+        preturn (new ResizeArray<_>())
+    ]
+
+/// Parses a list of 0 or more directives.
+let directives =
+    let directive =
+        %% '@' -- +.name
+        -..- +.optionalMany arguments
+        -%> fun name args ->
+            { DirectiveName = name; Arguments = args }
+    qty.[0..] /. ignored * withSource directive
+
+(**
+
+Fragments define reusable selections of fields.
+They can be given names, which are allowed to be anything except "on".
+We'll add this validation to our usual `name` parser.
+
+*)
+
+let fragmentName =
+    name >>= fun name ->
+        if name = "on" then fail "fragment name may not be `on`"
+        else preturn name
+
+(**
+
+A fragment spread references a named fragment, bringing its fields into the current selection scope.
+
+It looks like:
+
+    ...fragmentName
+
+Or, with directives:
+
+    ...fragmentName @dir1 @dir2(arg1:value1)
+
+*)
+
+let fragmentSpread =
+    %% "..."
+    -..- +.name
+    -..- +.optionalMany directives
+    -%> fun name dirs ->
+        { FragmentName = name; Directives = dirs } : FragmentSpread
+
+(**
+
+A field within a selection looks like `user(id: 10) { name, email }`.
+
+We could define a `Field` parser if we had a parser for selections.
+We'll leave it as a parameter for now.
+
+*)
+
+let field selections : Parser<_, _> =
+    let alias = %% +.name -- ignored -? ':' -- ignored -%> auto
+    %% +.(alias * zeroOrOne)
+    -- +.name
+    -..- +.optionalMany arguments
+    -..- +.directives
+    -..- +.optionalMany selections
+    -%> fun alias name args dirs sels ->
+        {
+            Alias = alias
+            FieldName = name
+            Arguments = args
+            Directives = dirs
+            Selections = sels
+        }
+
+(**
+
+An inline fragment looks like `...on User { name, email }`.
+
+Again, this needs a selection set parser to define, which we'll take
+as a parameter to the inline fragment parser.
+
+*)
+
+let typeCondition =
+    %% "on" -..- +.name -%> auto
+
+let inlineFragment selections =
+    %% "..."
+    -..- +.(typeCondition * zeroOrOne)
+    -..- +.directives
+    -..- +.selections
+    -%> fun typeCond dirs sels ->
+        {
+            TypeCondition = typeCond
+            Directives = dirs
+            Selections = sels
+        } : InlineFragment
+
+(**
+
+Now that we have fields and inline fragements, we can define a recursive parser
+for selections.
+
+*)
+
+let selections = precursive <| fun selections ->
+    let selection =
+        %% +.[
+            %% +.field selections -%> FieldSelection
+            %% +.fragmentSpread -%> FragmentSpreadSelection
+            %% +.inlineFragment selections -%> InlineFragmentSelection
+        ] -- ignored -%> auto
+    %% '{'
+    -..- +.(qty.[1..] /. ignored * withSource selection)
+    -- '}'
+    -%> auto
+
+(**
+
+Types are simple enough to parse. They're just names or lists of names.
+The one tricky part is that we shouldn't allow "nullable nullable" types.
+
+This is why `coreTypeDescription` and `typeDescription` are separate.
+
+*)
+
+let coreTypeDescription typeDescription =
+    let listType =
+        %% '[' -..- +.(qty.[1..] * typeDescription) -..- ']' -%> ListType
+    let namedType =
+        %% +.name -%> NamedType
+    %[
+        listType
+        namedType
+    ]
+
+let typeDescription = precursive <| fun typeDescription ->
+    %% +.coreTypeDescription typeDescription
+    -..- +.('!' * zeroOrOne)
+    -- ignored
+    -%> fun desc bang ->
+        {
+            Type = desc
+            Nullable = bang <> None
+        }
+
+(**
+
+Variable definitions look like `($var1: type1 = defaultVal)`.
+There's nothing special about these -- we're just gluing together parsers
+we've already defined.
+
+*)
+
+let defaultValue =
+    %% '='
+    -..- +.valueConst
+    -%> auto
+
+let variableDefinition =
+    %% +.variableName
+    -..- ':'
+    -..- +.typeDescription
+    -..- +.(defaultValue * zeroOrOne)
+    -%> fun variable ty defaultVal ->
+        {
+            VariableName = variable
+            Type = ty
+            DefaultValue = defaultVal
+        }
+
+let variableDefinitions =
+    %% '('
+    -..- +.(qty.[1..] /. ignored * withSource variableDefinition)
+    -- ')'
+    -%> auto
+(**
+
+Parsing operation types is similar to parsing booleans.
+
+*)
+
+let operationType =
+    %[
+        %% "query" -%> Query
+        %% "mutation" -%> Mutation
+    ]
+
+(**
+
+Long-hand operations have many optional elements, but again, we're just gluing
+together parsers we've already defined, which is pretty easy.
+
+*)
+
+let longhandOperation =
+    %% +.operationType
+    -..- +.(name * zeroOrOne)
+    -..- +.optionalMany variableDefinitions
+    -..- +.optionalMany directives
+    -..- +.optionalMany selections
+    -%> fun ty name varDefs dirs sels ->
+        {
+            Type = ty
+            Name = name
+            VariableDefinitions = varDefs
+            Directives = dirs
+            Selections = sels
+        }
+
+let operation =
+    %[
+        %% +.selections -%> ShorthandOperation
+        %% +.longhandOperation -%> LonghandOperation
+    ]
+
+(**
+
+Fragments defintions are pretty similar to long-hand operations.
+
+*)
+
+let fragment =
+    %% "fragment"
+    -..- +.fragmentName
+    -..- +.typeCondition
+    -..- +.optionalMany directives
+    -..- +.selections
+    -%> fun name typeCond dirs sels ->
+        {
+            FragmentName = name
+            TypeCondition = typeCond
+            Directives = dirs
+            Selections = sels
+        }
+
+(**
+
+A definition may be either an operation or a fragment.
+
+*)
+
+let definition : Parser<Definition, unit> =
+    %[
+        %% +.operation -%> OperationDefinition
+        %% +.fragment -%> FragmentDefinition
+    ]
+
+(**
+
+When parsing a whole document we need to be careful to allow ignored tokens
+not just between definitions, but before the first definition. This is the only time we
+should ever parse ignored tokens at the *start* of a parser. Parsers that aren't
+at the top level should expect that any preceding ignored tokens have already
+been consumed.
+
+*)
+
+let document =
+    %% ignored
+    -- +.(qty.[0..] /. ignored * withSource definition)
+    -%> fun defs -> { Definitions = defs }
---
+++ GraphQL.Parser/ParserAST.fs
@@ -0,0 +1,224 @@
+﻿//MIT License
+//
+//Copyright (c) 2016 Robert Peele
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
+
+namespace GraphQL.Parser.ParserAST
+open System.Collections.Generic
+
+/// The position in the source file that a syntactic element appeared.
+type SourcePosition =
+    {
+        Index : int64
+        Line : int64
+        Column : int64
+    }
+
+/// The span of (start, end) positions in the source file
+/// that a syntactic element occupies.
+type SourceInfo =
+    {
+        StartPosition : SourcePosition
+        EndPosition : SourcePosition
+    }
+
+/// `'a` with the positions in source that it spanned.
+type WithSource<'a> =
+    {
+        /// The position in source of the syntactic element
+        Source : SourceInfo
+        /// The syntactic element
+        Value : 'a
+    }
+
+// Most of the time when we need to put source info on an AST element
+// it's because it's within a list, therefore the SourceInfo of its containing
+// element will be too general to direct the user to the problem location.
+// Because of how common this is, we define the type alias `ListWithSource` for a list of
+// elements with source info on each one.
+type ListWithSource<'a> = ResizeArray<'a WithSource>
+
+/// String alias for a variable name.
+/// Variable names in the AST do *not* contain a leading "$".
+type VariableName = string
+
+/// String alias for a field name in an object literal or selection.
+/// May be any identifier.
+type FieldName = string
+
+/// Represents a GraphQL value.
+/// This may be a variable name, a literal, or a list/object
+/// containing more values.
+type Value =
+    | Variable of VariableName
+    | IntValue of int64
+    | FloatValue of double
+    | StringValue of string
+    | BooleanValue of bool
+    | EnumValue of string
+    | ListValue of Value ListWithSource
+    | ObjectValue of IDictionary<FieldName, Value WithSource>
+
+/// String alias for an argument name, which may be any identifier.
+type ArgumentName = string
+/// String alias for a directive name, which may be any identifier.
+type DirectiveName = string
+/// String alias for a fragment name, which may be any identifier except `on`.
+type FragmentName = string
+/// String alias for a type name.
+type TypeName = string
+/// String alias for an operation name.
+type OperationName = string
+
+/// Represents an `argName: argValue` pair passed as an argument
+/// to a directive or field.
+/// See arguments in section 2.6 of the GraphQL spec.
+type Argument =
+    {
+        ArgumentName : ArgumentName
+        ArgumentValue : Value
+    }
+
+/// Represents a directive, e.g. `@dirName(arg1: val1, arg2: val2)`.
+type Directive =
+    {
+        DirectiveName : DirectiveName
+        Arguments : Argument ListWithSource
+    }
+
+/// Represents a usage of a named fragment via the spread operator `...fragmentName`.
+type FragmentSpread =
+    {
+        FragmentName : FragmentName
+        Directives : Directive ListWithSource
+    }
+
+/// Represents a field within a selection, e.g. `user(id: 10) { name, email }`.
+/// The field may have arguments, directives, and sub-selections.
+/// See fields in section 2.5 of the GraphQL spec.
+type Field =
+    {
+        /// If an alias is supplied, the response object will return data
+        /// in a field with this name instead of `FieldName`.
+        /// This is useful when the same field name is used with different arguments.
+        Alias : FieldName option
+        FieldName : FieldName
+        Arguments : Argument ListWithSource
+        Directives : Directive ListWithSource
+        Selections : Selection ListWithSource
+    }
+/// Represents a single selection element.
+/// This may be a field, a fragment spread, or an inline fragment.
+/// See selection sets in section 2.4 of the GraphQL spec.
+and Selection =
+    | FieldSelection of Field
+    | FragmentSpreadSelection of FragmentSpread
+    | InlineFragmentSelection of InlineFragment
+/// Represents an inline fragment, which is used to apply a type condition
+/// or additional directives to some fields of a selection set.
+/// See inline fragments in section 2.8.2 of the GraphQL spec.
+and InlineFragment =
+    {
+        /// Only include these selections in the result for objects
+        /// of the named type. This feature only makes sense when
+        /// the GraphQL server can return objects of different runtime types
+        /// from the same query (perhaps different subclasses of the same base).
+        TypeCondition : TypeName option
+        Directives : Directive ListWithSource
+        Selections : Selection ListWithSource
+    }
+
+/// Represents a named type or list of (possibly nullable) types.
+/// This is separate from `TypeDescription` in order to make it impossible
+/// to describe a "nullable (not nullable (type))" or similar nonsense.
+type CoreTypeDescription =
+    | NamedType of TypeName
+    | ListType of TypeDescription ResizeArray
+/// Represents a GraphQL type.
+/// This may be a named type or a list of types and may be nullable.
+/// See types in section 3 of the GraphQL spec.
+and TypeDescription =
+    {
+        Type : CoreTypeDescription
+        Nullable : bool
+    }
+
+/// Represents the declaration of a variable, e.g. `$myVar: Int = 0`.
+/// See variables in section 2.10 of the GraphQL spec.
+type VariableDefinition =
+    {
+        /// The name of the variable being declared.
+        /// Note that it does *not* contain a leading "$".
+        VariableName : VariableName
+        Type : TypeDescription
+        /// If present, a default value for the variable.
+        DefaultValue : Value option
+    }
+
+/// Distinguishes between mutation and query operation types.
+type OperationType =
+    | Query
+    | Mutation
+
+/// Represents a mutation or query operation in long-hand form, which
+/// means it can be named, can have variable definitions, and can
+/// have directives applied to it.
+type LonghandOperation =
+    {
+        Type : OperationType
+        Name : OperationName option
+        VariableDefinitions : VariableDefinition ListWithSource
+        Directives : Directive ListWithSource
+        Selections : Selection ListWithSource
+    }
+
+/// Represents an operation.
+/// See operations in section 2.3 of the GraphQL spec.
+type Operation =
+    /// An anonymous shorthand query consisting only of a selection list.
+    | ShorthandOperation of Selection ListWithSource
+    /// A query or mutation, possibly with directives, variables, etc.
+    | LonghandOperation of LonghandOperation
+
+/// Represents the declaration of a fragment.
+/// A fragment is a set of selections on a type that can be reused by name in
+/// the document. It may also have directives applied to the selections.
+/// See fragments in section 2.8 of the GraphQL spec.
+type Fragment =
+    {
+        /// The name of the fragment.
+        FragmentName : FragmentName
+        /// The type name these selections are intended to be made on.
+        TypeCondition : TypeName
+        Directives : Directive ListWithSource
+        Selections : Selection ListWithSource
+    }
+
+/// Represents the declaration of either an operation or a named fragment.
+/// A GraphQL document is just a list of these.
+type Definition =
+    | OperationDefinition of Operation
+    | FragmentDefinition of Fragment
+
+/// Represents a complete GraphQL document.
+type Document =
+    {
+        Definitions : Definition ListWithSource
+    }
